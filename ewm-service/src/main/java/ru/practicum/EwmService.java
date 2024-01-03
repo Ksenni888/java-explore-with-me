3ccaafa5066bbbb90ca52896ae33fb59672a14aa
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2024-01-03 21:37:58 +0300

    pom 1

--- ewm-service/src/main/java/ru/practicum/EwmService.java
+++ ewm-service/src/main/java/ru/practicum/EwmService.java
@@ -1,27 +1,27 @@
-package ru.practicum;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-
-import java.time.LocalDateTime;
-import java.util.Random;
-
-@RequiredArgsConstructor
-@Service
-public class EwmService {
-
-    private final StatsClient client;
-
-    public Integer getEvent() {
-        StatsDto statsDto = new StatsDto();
-        statsDto.setApp("ewm-main-service");
-        statsDto.setIp("192.163.0.1");
-        statsDto.setUri("/events/1");
-        statsDto.setTimestamp(LocalDateTime.now());
-
-        Random random = new Random();
-        client.saveStats(statsDto);
-        return random.nextInt();
-    }
-}
+//package ru.practicum;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//
+//import java.time.LocalDateTime;
+//import java.util.Random;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class EwmService {
+//
+//    private final StatsClient client;
+//
+//    public Integer getEvent() {
+//        StatsDto statsDto = new StatsDto();
+//        statsDto.setApp("ewm-main-service");
+//        statsDto.setIp("192.163.0.1");
+//        statsDto.setUri("/events/1");
+//        statsDto.setTimestamp(LocalDateTime.now());
+//
+//        Random random = new Random();
+//        client.saveStats(statsDto);
+//        return random.nextInt();
+//    }
+//}
